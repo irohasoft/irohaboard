59c5453ee44b19c98aeae4f7d90f5992cc5fb8d0
Kotaro Miura <miura@irohasoft.jp>
2017-07-21 20:23:20 +0900

    Removed unnecessary lines

--- Config/update.sql
+++ Config/update.sql
@@ -9,14 +9,3 @@ CREATE TABLE IF NOT EXISTS `ib_infos_groups` (
   `comment` text,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS `ib_infos_groups3` (
-  `id` int(8) NOT NULL AUTO_INCREMENT,
-  `info_id` int(8) NOT NULL DEFAULT '0',
-  `group_id` int(8) NOT NULL DEFAULT '0',
-  `created` datetime DEFAULT NULL,
-  `modified` datetime DEFAULT NULL,
-  `comment` text,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-

5a2cfdfe95bf6315756f88338979f0de22c03032
TachibanaET <guoenfu321@gmail.com>
2019-09-13 18:43:02 +0200

    アンケート記入・表示機能を実装

--- Model/Soap.php
+++ Model/Soap.php
@@ -125,18 +125,66 @@ class Soap extends AppModel
 	public $filterArgs = array(
 	);
 
-	// 過去4回分のSOAPを取得
+  public function findGroup(){
+    $sql = "SELECT id,title FROM ib_groups";
+    $data = $this->query($sql);
+    //$this->log($data);
+    return $data;
+  }
+
+  public function findAllUserInGroup( $group_id ){
+    $sql = "SELECT id, group_id FROM ib_users
+            WHERE group_id = $group_id";
+    $data = $this->query($sql);
+    return $data;
+  }
+
+  public function getGroupList(){
+    $sql = "SELECT id,title FROM ib_groups ";
+    $data = $this->query($sql);
+    return $data;
+  }
+
+  public function getUserList(){
+    $sql = "SELECT id, username, name, pic_path FROM ib_users WHERE role = 'user' ORDER BY username ASC";
+    $data = $this->query($sql);
+    return $data;
+  }
+
+  public function findUserGroup($user_id){
+    $sql = "SELECT group_id FROM ib_users WHERE id = $user_id";
+    $data = $this->query($sql);
+    //$this->log($data);
+    return $data[0]['ib_users']['group_id'];
+  }
+
+  public function findUserList($username, $name){
+    if($username != null){
+      $username = "%".$username."%";
+    }
+    if($name != null){
+      $name = "%".$name."%";
+    }
+    if($username == null && $name == null){
+      $username = "%".$username."%";
+      $name = "%".$name."%";
+    }
+    $sql = "SELECT id, username, name, pic_path FROM ib_users
+        WHERE (username LIKE '$username'
+          OR name LIKE '$name')
+          AND (role = 'user')
+        GROUP BY username
+        ORDER BY username ASC";
+    $data = $this->query($sql);
+    return $data;
+  }
+
 	public function findRecentSoaps($user_id){
-		$data = $this->find('all', array(
-			'fields' => array(
-				'id', 'user_id', 'current_status', 'studied_content', 'S', 'O', 'A', 'P', 'created'
-			),
-			'conditions' => array('user_id' => $user_id),
-			'order' => array('created' => 'desc'),
-			'limit' => 4,
-			'recursive' => -1
-		));
-		//$this->log($data);
+		$sql = "SELECT id, user_id, current_status, S, O, A, P, created FROM ib_soaps
+				WHERE (user_id = '$user_id')
+				ORDER BY created DESC
+				LIMIT 4";
+		$data = $this->query($sql);
 		return $data;
 	}
 
@@ -145,7 +193,7 @@ class Soap extends AppModel
 		if (empty($members)){ return NULL; }
 		$members_recent_soaps = array();
 		foreach($members as $member):
-			$user_id = $member['User']['id'];
+			$user_id = $member['ib_users']['id'];
 			$recent_soaps = $this->findRecentSoaps($user_id);
 			$members_recent_soaps += [$user_id => $recent_soaps];
 		endforeach;
@@ -153,13 +201,9 @@ class Soap extends AppModel
 	}
 
 	public function getOldestCreatedYear(){
-		$data = $this->find('first', array(
-			'fields' => array(
-				'MIN(created) AS oldest_created_time',
-			),
-			'recursive' => -1
-		));
-		$oldest_created_year = (new DateTime($data[0]['oldest_created_time']))->format('Y');
+		$sql = "SELECT MIN(created) AS oldest_created_time FROM ib_soaps";
+		$data = $this->query($sql);
+		$oldest_created_year = (new DateTime($data[0]['ib_soaps']['oldest_created_time']))->format('Y');
 		//$this->log($oldest_created_year);
 		return $oldest_created_year;
 	}

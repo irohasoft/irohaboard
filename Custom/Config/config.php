c66327f48a0b7374cfb196543c3637b1b7e69576
Kotaro Miura <miura@irohasoft.jp>
2020-09-09 11:31:58 +0900

    Add a configuration example

--- Custom/Config/config.php
+++ Custom/Config/config.php
@@ -10,3 +10,17 @@
 
 // Custom Config
 $config['dummy']		= array();
+
+/*
+// PC向け理解度ボタンラベル
+Configure::delete('record_understanding_pc');
+$config['record_understanding_pc'] = array(
+	'1'		=> '終了'
+);
+
+// スマートフォン向け理解度ボタンラベル
+Configure::delete('record_understanding_spn');
+$config['record_understanding_spn'] = array(
+	'1'		=> '終了'
+);
+*/

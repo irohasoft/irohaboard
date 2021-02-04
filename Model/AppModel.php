ba3beeb31b7a17f22fa081508217e3c204bfa0f4
Kotaro Miura <miura@irohasoft.jp>
2021-02-04 12:58:29 +0900

    不要なコードを削除、コメントを追加

--- Model/AppModel.php
+++ Model/AppModel.php
@@ -20,12 +20,13 @@ App::uses('Model', 'Model');
  */
 class AppModel extends Model
 {
-	private $result = null;
-	private $type = 'first';
-	private $query = [];
+	private $params = []; // メソッドチェーン を使用した場合に find() で使用するパラメータ
 	
 	/**
 	 * 英数字チェック（マルチバイト対応）
+	 * 
+	 * @param array $check チェック対象
+	 * @return bool OK:true, NG:false
 	 */
 	public function alphaNumericMB($check)
 	{
@@ -35,78 +36,107 @@ class AppModel extends Model
 		return preg_match('/^[a-zA-Z0-9]+$/', $value);
 	}
 
+	/**
+	 * findById() と同様の仕様
+	 */
 	public function get($id)
 	{
 		return $this->findById($id);
 	}
 
-	public function find($type = null, $query = [])
+	/**
+	 * 既存の find() メソッドを上書き
+	 * 
+	 * @param string $type 取得形式（指定した場合は、通常の動きとなる。省略した場合はメソッドチェーンを利用し、最後に all(), fisrt() をつけてデータを取得）
+	 * @param array $params 各種条件（$type を指定した場合のみ指定可能）
+	 * @return array type を指定した場合は取得結果、省略した場合はメソッドチェーン用にインスタンスを返す
+	 */
+	public function find($type = null, $params = [])
 	{
 		if($type == null)
 		{
-			$this->type = $type;
-			$this->query = $query;
-			
+			$this->params = [];
 			return $this;
 		}
 		
-		return parent::find($type, $query);
+		return parent::find($type, $params);
 	}
 
-	public function select($param)
+	/**
+	 * フィールドを指定
+	 */
+	public function select($value)
 	{
-		$this->query['fields'] = $param;
-		
+		$this->params['fields'] = $value;
 		return $this;
 	}
 
-	public function where($param)
+	/**
+	 * where 句を指定
+	 */
+	public function where($value)
 	{
-		$this->query['conditions'] = $param;
-		
+		$this->params['conditions'] = $value;
 		return $this;
 	}
 
-	public function order($param)
+	/**
+	 * ソート順を指定（文字列もしくは配列で指定）
+	 */
+	public function order($value)
 	{
-		$this->query['order'] = $param;
-		
+		$this->params['order'] = $value;
 		return $this;
 	}
 
-	public function group($param)
+	/**
+	 * group by を指定
+	 */
+	public function group($value)
 	{
-		$this->query['group'] = $param;
-		
+		$this->params['group'] = $value;
 		return $this;
 	}
 
-	public function limit($param)
+	/**
+	 * データの取得数を指定
+	 */
+	public function limit($value)
 	{
-		$this->query['limit'] = $param;
-		
+		$this->params['limit'] = $value;
 		return $this;
 	}
 
-	public function page($param)
+	/**
+	 * ページ番号を指定
+	 */
+	public function page($value)
 	{
-		$this->query['page'] = $param;
-		
+		$this->params['page'] = $value;
 		return $this;
 	}
 
+	/**
+	 * find('all')の結果を返す
+	 */
 	public function all()
 	{
-		return parent::find('all', $this->query);
+		return parent::find('all', $this->params);
 	}
 
+	/**
+	 * find('first')の結果を返す
+	 */
 	public function first()
 	{
-		return parent::find('first', $this->query);
+		return parent::find('first', $this->params);
 	}
 
+	/**
+	 * find('count')の結果を返す
+	 */
 	public function count()
 	{
-		return parent::find('count', $this->query);
+		return parent::find('count', $this->params);
 	}
 }

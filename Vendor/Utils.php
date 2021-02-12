fa45d86115cbc0726ba1e1c63c0acca6d674fe0b
Kotaro Miura <miura@irohasoft.jp>
2021-02-12 16:29:33 +0900

    コメントを追加

--- Vendor/Utils.php
+++ Vendor/Utils.php
@@ -14,16 +14,34 @@ class Utils
 	{
 	}
 	
+	/**
+	 * 対象日時から時間を切り取って返す
+	 * 
+	 * @param string $str 対象日時（YYYY-MM-DD HH:MM:SS）
+	 * @return string 日付 YYYY-MM-DD
+	 */
 	public static function getYMD($str)
 	{
 		return substr($str, 0, 10);
 	}
 
+	/**
+	 * 対象日時から秒を切り取って返す
+	 * 
+	 * @param string $str 対象日時（YYYY-MM-DD HH:MM:SS）
+	 * @return string 日付 YYYY-MM-DD HH:MM
+	 */
 	public static function getYMDHN($str)
 	{
 		return substr($str, 0, 16);
 	}
 	
+	/**
+	 * 秒を時間に変換
+	 * 
+	 * @param int $sec 秒
+	 * @return int 時間 HH:MM:SS
+	 */
 	public static function getHNSBySec($sec)
 	{
 		$hour	= floor($sec / 3600);
@@ -35,12 +53,18 @@ class Utils
 		return $hms;
 	}
 	
+	/**
+	 * CSVデータを取得
+	 * 
+	 * @param string $file_path CSVファイルのパス
+	 * @return array CSVデータ（2次元配列）
+	 */
 	public static function getCsvData($file_path)
 	{
 		setlocale(LC_ALL, 'ja_JP.UTF-8');
 		
 		$data = file_get_contents($file_path);
-		$data = mb_convert_encoding($data, 'UTF-8', 'sjis-win');
+		$data = mb_convert_encoding($data, 'UTF-8', 'SJIS-Win');
 		$temp = tmpfile();
 		$meta = stream_get_meta_data($temp);
 		
@@ -52,7 +76,8 @@ class Utils
 		
 		$csv  = [];
 		 
-		foreach($file as $line) {
+		foreach($file as $line)
+		{
 			$csv[] = $line;
 		}
 		
@@ -68,6 +93,12 @@ class Utils
 	}
 	*/
 	
+	/**
+	 * 設定項目の値に対応するキーを取得
+	 * 
+	 * @param string $configure 設定の項目名
+	 * @return string 値
+	 */
 	public static function getKeyByValue($configure, $value)
 	{
 		$list = Configure::read($configure);
@@ -81,6 +112,13 @@ class Utils
 		return null;
 	}
 	
+	/**
+	 * 連想配列内の値に対応するキーを取得
+	 * 
+	 * @param array $list 対象配列
+	 * @param array $title 値
+	 * @return string キー
+	 */
 	public static function getIdByTitle($list, $title)
 	{
 		foreach ($list as $key => $value)
@@ -92,6 +130,12 @@ class Utils
 		return null;
 	}
 	
+	/**
+	 * 指定した桁数のパスワードを生成
+	 * 
+	 * @param int $digit 桁数
+	 * @return string 生成したパスワード
+	 */
 	public static function getNewPassword($digit)
 	{
 		return substr(str_shuffle('23456789abcdefghijkmnopqrstuvwxyz'), 0, $digit);

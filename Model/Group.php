e3fb0b5d7958dd0a539610e03f2f7673c78c32ce
Kotaro Miura <miura@irohasoft.jp>
2021-02-12 15:11:04 +0900

    不要なメソッドを削除

--- Model/Group.php
+++ Model/Group.php
@@ -63,36 +63,10 @@ class Group extends AppModel
 	public function getUserIdByGroupID($group_id)
 	{
 		$sql = "SELECT user_id FROM ib_users_groups WHERE group_id = :group_id";
-		
 		$params = ['group_id' => $group_id];
-		
-		$data = $this->query($sql, $params);
-		
-		$list = [];
-		
-		for($i=0; $i< count($data); $i++)
-		{
-			$list[$i] = $data[$i]['ib_users_groups']['user_id'];
-		}
-		
+
+		$list = $this->query_list($sql, $params, 'ib_users_groups', 'user_id');
+
 		return $list;
-	}
-	
-	/**
-	 * グループ一覧を取得
-	 * 
-	 * @return array グループ一覧
-	 */
-	public function getGroupList()
-	{
-		$groups = $this->find()-all();
-		$data   = ["0" => "全て"];
-		
-		for($i=0; $i< count($groups); $i++)
-		{
-			$data[''.$groups[$i]['Group']['id']] = $groups[$i]['Group']['title'];
-		}
-		
-		return $data;
 	}
 }

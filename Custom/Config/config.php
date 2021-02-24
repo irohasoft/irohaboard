94a76138374d7a8db112ea6d4dc26eed7eed4959
Kotaro Miura <miura@irohasoft.jp>
2021-02-24 10:55:12 +0900

    管理システムのログイン用リンクをデフォルトで無効とする

--- Custom/Config/config.php
+++ Custom/Config/config.php
@@ -2,6 +2,9 @@
 // Custom Config
 $config['dummy']		= [];
 
+// 受講者ログイン画面に管理者ログイン用リンクを表示する場合
+//$config['show_admin_link'] = true;
+
 /*
 // PC向け理解度ボタンラベル
 Configure::delete('record_understanding_pc');

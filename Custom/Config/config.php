93944269610b5969b7b06fad3f1371431c5cfc74
Kotaro Miura <miura@irohasoft.jp>
2021-03-08 12:16:39 +0900

    設定に関する説明を追加

--- Custom/Config/config.php
+++ Custom/Config/config.php
@@ -2,9 +2,11 @@
 // Custom Config
 $config['dummy']		= [];
 
-// 受講者ログイン画面に管理者ログイン用リンクを表示する場合
+// 受講者ログイン画面に管理者ログイン用リンクを表示する場合はコメントを解除してください。
 //$config['show_admin_link'] = true;
 
+
+// 学習画面の理解度ボタンを「終了」と「中断」ボタンのみとする場合はコメントを解除してください。
 /*
 // PC向け理解度ボタンラベル
 Configure::delete('record_understanding_pc');

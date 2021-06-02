ced8a8e3b5ea807de6ac2461a0b46947cf5e75eb
Kotaro Miura <miura@irohasoft.jp>
2021-06-02 19:28:51 +0900

    コメントを追加

--- Config/database.php
+++ Config/database.php
@@ -1,18 +1,20 @@
 <?php
 class DATABASE_CONFIG
 {
+	// iroha Board で使用するデータベース
 	public $default = [
-		'datasource' => 'Database/Mysql',
+		'datasource' => 'Database/Mysql', // 変更しないでください
 		'persistent' => true,
-		'host' => 'localhost',
-		'login' => 'root',
-		'password' => '',
-		'database' => 'irohaboard',
-		'prefix' => 'ib_',
+		'host' => 'localhost', // MySQLサーバのホスト名
+		'login' => 'root', // ユーザ名
+		'password' => '', // パスワード
+		'database' => 'irohaboard', // データベース名
+		'prefix' => 'ib_', // 変更しないでください
 		'encoding' => 'utf8'
 	];
 
 /*
+	// ユニットテストで使用するデータベース（注意：データが初期化されます）
 	public $test = [
 		'datasource' => 'Database/Mysql',
 		'persistent' => true,
